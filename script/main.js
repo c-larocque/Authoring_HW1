7144491916815755c71c8ac70755b989f3905f18
c-larocque <clarocque1@live.ca>
2017-09-25 17:45:26 -0400

    add code for lightbox

--- script/main.js
+++ script/main.js
@@ -28,6 +28,11 @@
           // set the src
           newSubImg.src = "images/" + objectIndex.images[index];
 
+          newSubImg.dataset.index = index;
+
+          //add an event handler to trigger a lightbox
+          newSubImg.addEventListener('click', popLightbox, false);
+
           // add the image to the page
           subImages.appendChild(newSubImg);
         });
@@ -51,6 +56,15 @@
         //add an event handler to each image
         image.addEventListener('click', changeElements, false);
       });
+
+      //trigger the lightbox
+      function popLightbox() {
+        //debugger;
+        //trigger the lightbox overlay so that we can see it
+        let lightbox = document.querySelector('.lightbox');
+
+        lightbox.style.display = 'block';
+      }
 
       //document.querySelector('#spring').click();
       changeElements.call(document.querySelector('#spring'));
